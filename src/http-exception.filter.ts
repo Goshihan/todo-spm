061b9d4d07eeba8c6fcb346c84c0ab025bfa4939
goshihan06@gmail.com <goshihan06@gmail.com>
2024-09-01 18:27:47 +0530

    first NextJs Front

---
+++ src/http-exception.filter.ts
@@ -0,0 +1,30 @@
+import { ExceptionFilter,Catch,ArgumentsHost,HttpException,Logger } from "@nestjs/common";
+import { Request,Response } from "express"; 
+import path from "path";
+import { timestamp } from "rxjs";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter{
+    private readonly logger =new Logger(HttpExceptionFilter.name);
+
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx=host.switchToHttp();
+        const response =ctx.getResponse<Response>();
+        const request =ctx.getRequest<Request>();
+        const status=exception.getStatus();
+        const res=exception.getResponse();
+        this.logger.error(res);
+
+        response
+        .status(status)
+        .json({
+            statusCode:status,
+            timestamp:new Date().toISOString(),
+            path:request.url,
+            message:res['message']
+        });
+    }
+
+
+
+}
